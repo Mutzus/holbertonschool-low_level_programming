85b58e4f504159f584f60ef782872dfa21a8d618
Mutzus <ssmutzus@gmail.com>
2023-10-20 12:55:49 -0700

    Fix adding a description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message using puts.
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
