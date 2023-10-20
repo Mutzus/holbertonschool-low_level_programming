9a06c687087b152185d0d9835b23c846c6e48c72
Mutzus <ssmutzus@gmail.com>
2023-10-20 13:00:35 -0700

    Fix modifying structure

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-	printf("with proper grammar, but the outcome is a piece of art\n")
-			return 0;
-			}
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message using printf.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art")
+		return (0);
+}
