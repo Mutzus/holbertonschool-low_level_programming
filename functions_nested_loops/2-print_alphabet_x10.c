6277aec3e08759b2f5060f15673b5e2e13832307
Mutzus <ssmutzus@gmail.com>
2023-10-21 13:33:52 -0700

    Fixing Betty style details

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,6 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		char letter = 'a';
-		
 		while (letter <= 'z')
 		{
 			_putchar(letter);
