121e82a1921ec1ee2e6c41cd95ff06f04b9f9cd3
Mutzus <ssmutzus@gmail.com>
2023-10-21 00:57:27 -0700

    Fixing Betty details

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-        char letter = 'a';
-        while (letter <= 'z')
-        {
+	char letter = 'a';
+	while (letter <= 'z')
+	{
 		if (letter != 'q' && letter != 'e')
 		{
 			putchar(letter);
