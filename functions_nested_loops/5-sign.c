9f8994cd8fdd7b6f5a61cb6f4ba0562627980830
Mutzus <ssmutzus@gmail.com>
2023-10-21 14:02:50 -0700

    Fixing logic on if

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,12 +8,21 @@
 
 int print_sign(int n)
 {
-	if (n <= -1)
+	if (n >= 0)
 	{
-	return (-1);
+		if (n > 0)
+		{
+			_putchar('+');
+		}
+		else
+		{
+			_putchar('0');
+			return (0);
+		}
 	}
 	else
 	{
-	return (n >= 1);
+		_putchar('-');
 	}
-}	
+	return (1);
+}
