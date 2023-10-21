6d64471bd5c6e353984e3404020fac0bf8546594
Mutzus <ssmutzus@gmail.com>
2023-10-20 20:10:31 -0700

    Fix modifying description

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - Prints if n is +, - or 0
+ *
+ * Return: Always 0 (Success)
+ */
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -11,14 +17,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	/**
- * main - Entry point of the program
- *
- * Description: Using function if to get if the value of n is p, n or z.
- *
- * Return: Always 0 (success)
- */
-
 	if (n >= 0)
 	{
 		if (n == 0)
