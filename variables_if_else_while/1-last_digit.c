91a5750b4bec050b5548f0ff7cda2f40b002807b
Mutzus <ssmutzus@gmail.com>
2023-10-21 00:24:11 -0700

    Fixing logic on if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,28 +11,27 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-        int n;
+	int n;
 	int lastDigit;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	lastDigit = n % 10;
-        if (lastDigit >= 6)
-        {
-                if (n == 0)
-                {
-                        printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-                }
-                else
-                {
-                        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-                }
-        }
-        else
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-        }
-
-        return (0);
+	if (lastDigit >= 6)
+	{
+		if (n == 0)
+		{
+			printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		}
+		else
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		}
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	return (0);
 }
