1ee82bd5b175481cd0cf152c3581315d8055c8cd
Mutzus <ssmutzus@gmail.com>
2023-10-22 23:19:54 -0700

    Fixing compiling errors

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 			}
 			if (x < 10)
 			{
-				_putchar('10');
+				_putchar(' ');
 			}
 			else
 			{
@@ -33,6 +33,7 @@ void times_table(void)
 			{
 				_putchar(',');
 			}
-			_putchar('\n')
 		}
+		_putchar('\n');
+	}
 }
