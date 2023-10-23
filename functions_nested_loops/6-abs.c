419f7bc2bd91b6fc19f9182b59f14b04a015368c
Mutzus <ssmutzus@gmail.com>
2023-10-22 22:40:53 -0700

    Uploading Task 6-8

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _abs - prints the absolute value of an integer
+ * @n: The character to be checked
+ * Return: absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return(-n);
+	}
+	else
+	{
+		return(n);
+	}
+}
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <"main.h">
+
+/**
+ * print_last_digit - Prints the last digit
+ * @n: character to be checked
+ * Return: The last digit
+ */
+
+int print_last_digit(int n)
+{
+	int lastD;
+
+	lastD = n % 10;
+	_putchar(lastD + '0');
+	return (lastD);
+}
