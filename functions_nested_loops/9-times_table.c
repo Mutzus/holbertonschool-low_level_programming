9f0e44c63a9db80ec3fc201e24c1eb5c08f534a5
Mutzus <ssmutzus@gmail.com>
2023-10-22 23:16:24 -0700

    Uploading task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,3 +1,38 @@
 #include "main.h"
 
+/**
+ * times_table - Prints 9 times table
+ *
+ * Return:
+ */
 
+void times_table(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (n = 0 ; i < 10 ; n++)
+		{
+			int x = n * n;
+
+			if (n != 0)
+			{
+				_putchar('0');
+			}
+			if (x < 10)
+			{
+				_putchar('10');
+			}
+			else
+			{
+				_putchar(x / 10 + '0');
+			}
+			_putchar(x % 10 + '0');
+			if (n < 9)
+			{
+				_putchar(',');
+			}
+			_putchar('\n')
+		}
+}
