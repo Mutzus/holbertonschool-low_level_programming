920eff46fe7c73c5f2706cd602bef85c200b97c0
Mutzus <ssmutzus@gmail.com>
2023-10-23 01:14:17 -0700

    Fix adding descending loop

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,34 @@
 #include <stdio.h>
 
 /**
- * void print_to_98 - Prints numbers to 98
- *
+ * print_to_98 - Prints numbers to 98
+ * @n: number to initiate
  * Return:
  */
 
 void print_to_98(int n)
 {
-	for (n = n; n <= 98; n++ )
+	if (n <= 98)
 	{
-		printf("%d", n);
-		if (n != 98)
+		for (n = n; n <= 98; n++)
 		{
-			printf(", ");
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	prinf("\n");
 }
