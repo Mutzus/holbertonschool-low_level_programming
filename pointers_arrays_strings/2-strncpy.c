0bb10cd61ee37548c109e4416c0a0b0cd825e8a1
Mutzus <ssmutzus@gmail.com>
2023-11-06 12:23:31 -0800

    Modifying description

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: destination char
- * @src: source char
- * @n: number of bytes
- * Return: The result
- */
+* _strncpy - copies string
+* @dest: destination string
+* @src: source string
+* @n: bytes from src
+* Return: the result
+*/
 
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 
