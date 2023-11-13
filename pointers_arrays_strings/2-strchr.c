093c761bf26981b0a1423e8114854ef262785d63
Mutzus <ssmutzus@gmail.com>
2023-11-12 21:02:05 -0800

    Fixing null return

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,25 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string.
  * @c: Character to locate.
  *
- * Return: A pointer to the first 
+ * Return: A pointer to the first
  * occurrence of the character c in the string s,
  * or NULL if the character is not found.
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
+		if ((*s == '\0'))
 		{
-			return (s);
+			return (NULL);
 		}
 		s++;
 	}
-	return ((char *)0);
+	return (s);
 }
