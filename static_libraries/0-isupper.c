75c6cc43c062c36e4aab865cb5958a022335ce2e
Mutzus <ssmutzus@gmail.com>
2023-11-20 11:54:14 -0800

    Fixing main.h file

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _isupper - Checks if c is a capital letter
+ * @c: The character to be checked
+ * Return: 1 for capital 0 otherwise
+ */
+
 int _isupper(int c)
 {
+	if (c < 'A' || c > 'Z')
+	{
+		return (0);
+	}
+	return (1);
 }
