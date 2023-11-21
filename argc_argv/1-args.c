8a179931ccec775976bb618d1e12487eaf9baa66
Mutzus <ssmutzus@gmail.com>
2023-11-20 19:16:32 -0800

    Removing unused parameter

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,10 @@
 /**
  * main - prints the number of arguments passed into it
  * @argc: argument count
- * @argv: argument vector
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(int argc)
 {
 	if (argc > 0)
 	{
