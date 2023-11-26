7156c20550a693b5dfc07b5c8aea6b3454ee4bb3
Mutzus <ssmutzus@gmail.com>
2023-11-25 19:19:04 -0800

    Fixing Betty issues

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,34 +8,30 @@
  * Return: A pointer to the newly allocated 2D array.
  *         Returns NULL on failure or if width or height is 0 or negative.
  */
-int **alloc_grid(int width, int height) {
-    int **grid;
-    int i, j;
 
-    /* Check if width or height is 0 or negative */
-    if (width <= 0 || height <= 0)
-        return NULL;
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
 
-    /* Allocate memory for the rows */
-    grid = (int **)malloc(height * sizeof(int *));
-    if (grid == NULL)
-        return NULL;
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    /* Allocate memory for the columns and initialize to 0 */
-    for (i = 0; i < height; i++) {
-        grid[i] = (int *)malloc(width * sizeof(int));
-        if (grid[i] == NULL) {
-            /* Free previously allocated memory if allocation fails */
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return NULL;
-        }
-
-        /* Initialize each element to 0 */
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
-
-    return grid;
+	grid = (int **)malloc(height * sizeof(int *);
+			if (grid == NULL)
+			return (NULL);
+			for (i = 0; i < height; i++)
+			{
+			grid[i] = (int *)malloc(width * sizeof(int));
+			if (grid[i] == NULL)
+			{
+			for (j = 0; j < i; j++)
+			free(grid[j]);
+			free(grid);
+			return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+			}
+			return (grid);
 }
--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,16 +5,15 @@
  * @grid: The pointer to the 2D array.
  * @height: The height of the grid.
  */
-void free_grid(int **grid, int height) {
-    int i;
 
-    /* Check if the grid pointer is not NULL */
-    if (grid != NULL) {
-        /* Free each row */
-        for (i = 0; i < height; i++)
-            free(grid[i]);
+void free_grid(int **grid, int height)
+{
+	int i;
 
-        /* Free the array of pointers to rows */
-        free(grid);
-    }
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
 }
