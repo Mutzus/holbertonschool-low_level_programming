0b356cef509de120d6b159fd3cbe86228468b054
Mutzus <ssmutzus@gmail.com>
2023-12-03 16:19:50 -0800

    Fixing Task 3

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
+#ifndef ABS_H
+#define ABS_H
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
-int main(void) {
-    int num = -5;
-    printf("Absolute value of %d is %d\n", num, ABS(num));
-
-    double num_double = -7.5;
-    printf("Absolute value of %.2f is %.2f\n", num_double, ABS(num_double));
-
-    return 0;
-}
+#endif /* ABS_H */
