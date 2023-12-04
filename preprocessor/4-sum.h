367c76f927bcb1d4928c5c6c26c870d2541c84ca
Mutzus <ssmutzus@gmail.com>
2023-12-03 16:21:36 -0800

    Fixing Task 4

--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
+#ifndef SUM_H
+#define SUM_H
 
 #define SUM(x, y) ((x) + (y))
 
-int main(void) {
-    int a = 5, b = 10;
-    printf("Sum of %d and %d is %d\n", a, b, SUM(a, b));
-
-    double x = 3.5, y = 2.5;
-    printf("Sum of %.2f and %.2f is %.2f\n", x, y, SUM(x, y));
-
-    return 0;
-}
+#endif /* SUM_H */
