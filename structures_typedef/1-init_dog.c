b60057b8dced6bdcef16c4ead950a30fd97e9b43
Mutzus <ssmutzus@gmail.com>
2023-12-03 16:41:57 -0800

    Fixing Betty Style

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,15 +2,19 @@
 
 /**
  * init_dog - Initializes a variable of type struct dog.
+ * dog - struct dog
  * @d: Pointer to the struct dog variable to initialize.
  * @name: Pointer to a string representing the dog's name.
  * @age: The age of the dog.
  * @owner: Pointer to a string representing the owner's name.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner) {
-    if (d != NULL) {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
-    }
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
