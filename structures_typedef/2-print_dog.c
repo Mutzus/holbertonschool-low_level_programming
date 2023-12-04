24c6fecbbc737328352702db755410efbebae451
Mutzus <ssmutzus@gmail.com>
2023-12-03 17:03:28 -0800

    Fixing Betty issues

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,10 +5,13 @@
  * print_dog - Prints the details of a struct dog.
  * @d: Pointer to the struct dog variable to print.
  */
-void print_dog(struct dog *d) {
-    if (d != NULL) {
-        printf("Name: %s\n", d->name ? d->name : "(nil)");
-        printf("Age: %.6f\n", d->age);
-        printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-    }
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
 }
