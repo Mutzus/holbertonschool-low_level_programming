e5a90822fd8766f4f4f37717bc20575facaa943f
Mutzus <ssmutzus@gmail.com>
2023-12-11 10:02:09 -0800

    Fixing Betty issues

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,44 +5,45 @@
  * print_all - Prints anything.
  * @format: List of types of arguments passed to the function
  */
+
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *separator = "";
-    char *str;
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
+	char *str;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-        case 'c':
-            printf("%s%c", separator, va_arg(args, int));
-            break;
-        case 'i':
-            printf("%s%d", separator, va_arg(args, int));
-            break;
-        case 'f':
-            printf("%s%f", separator, va_arg(args, double));
-            break;
-        case 's':
-            str = va_arg(args, char *);
-            if (str == NULL)
-                str = "(nil)";
-            printf("%s%s", separator, str);
-            break;
-        default:
-            i++;
-            continue;
-        }
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
 
-        separator = ", ";
-        i++;
-    }
+		separator = ", ";
+		i++;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
