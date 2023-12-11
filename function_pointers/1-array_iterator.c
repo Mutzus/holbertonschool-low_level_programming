151e44158003eeade800e78a6af320a4183f6132
Mutzus <ssmutzus@gmail.com>
2023-12-11 01:11:32 -0800

    Fixing Betty issues

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,15 +6,16 @@
  * @size: Size of the array.
  * @action: Pointer to the function to be executed on each element.
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array != NULL && action != NULL)
-    {
-        size_t i;
+	if (array != NULL && action != NULL)
+	{
+		size_t i;
 
-        for (i = 0; i < size; i++)
-        {
-            action(array[i]);
-        }
-    }
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
