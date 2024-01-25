9bdd397f79eaed361fa3f4225b59ee44fb6a7c5e
Mutzus <ssmutzus@gmail.com>
2024-01-25 08:22:31 -0800

    Fixing Betty issues

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,13 +9,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t element_count = 0;
+	size_t element_count = 0;
 
-    while (h != NULL)
-    {
-        element_count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		element_count++;
+		h = h->next;
+	}
 
-    return element_count;
+	return (element_count);
 }
