403649b864b02afb718c511eff9619363e20e7e2
Mutzus <ssmutzus@gmail.com>
2024-01-25 22:02:35 -0800

    Fixing Betty style issues

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,8 @@
-#include "lists.h"  // Assuming your header file is named lists.h
+#include "lists.h"
 
 /**
  * listint_len - Returns the number of elements in a linked listint_t list
  * @h: Pointer to the head of the list
- *
  * Return: The number of nodes in the list
  */
 size_t listint_len(const listint_t *h)
