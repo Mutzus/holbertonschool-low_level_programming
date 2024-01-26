3eca714c257735ef630f27fbe8c4a220267b11f3
Mutzus <ssmutzus@gmail.com>
2024-01-25 21:52:39 -0800

    Fixing Betty issues

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
-#include "lists.h"  // Assuming your header file is named lists.h
-#include <stdio.h>
+#include "lists.h"
 
 /**
  * print_listint - Prints all the elements of a listint_t list
@@ -9,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
