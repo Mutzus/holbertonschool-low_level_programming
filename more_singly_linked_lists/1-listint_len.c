4f4c3d3d87a0c5c78ede1f178b0c7dd3a5b8bd4a
Mutzus <ssmutzus@gmail.com>
2024-01-25 22:04:59 -0800

    Fixing Kernel Style issues

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -7,13 +7,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        node_count++;
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
