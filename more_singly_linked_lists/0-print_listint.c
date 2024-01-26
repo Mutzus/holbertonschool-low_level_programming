d1ef14cb8ed799bb9c2d0ee468fe5d2d7f866562
Mutzus <ssmutzus@gmail.com>
2024-01-25 21:58:59 -0800

    Fixing library issues

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
@@ -8,14 +8,14 @@ nclude "lists.h"
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
 
-    return (node_count);
+	return (node_count);
 }
