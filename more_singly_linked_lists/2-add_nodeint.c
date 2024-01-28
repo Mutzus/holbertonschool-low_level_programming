c1fd094743c401ba22ed452e22f7aa861a1b6335
Mutzus <ssmutzus@gmail.com>
2024-01-27 20:16:07 -0800

    Fixing Task 2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,35 +1,35 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t list
- * @head: Pointer to the head of the list
- * @n: Value to be stored in the new node
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to the pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
  *
- * Return: The address of the new element, or NULL if it failed
+ * Return: The address of the new element, or NULL if it failed.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+list_t *add_node(list_t **head, const char *str)
 {
-	listint_t *new_node, *temp;
+    list_t *new_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	return (NULL);
+    if (str == NULL)
+        return NULL;
 
-	new_node->n = n;
-	new_node->next = NULL;
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return NULL;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-		temp = temp->next;
-		temp->next = new_node;
-	}
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
 
-	return (new_node);
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return new_node;
 }
