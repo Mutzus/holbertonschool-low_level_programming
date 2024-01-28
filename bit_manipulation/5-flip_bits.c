4881ce2865fc23358082b907055f9b32fb0534a8
Mutzus <ssmutzus@gmail.com>
2024-01-27 22:09:52 -0800

    Fixing Betty Style Issues

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits needed to flip to get from one number to another.
+ * flip_bits - Returns the number of bits needed
+ * to flip to get from one number to another.
  * @n: The first number.
  * @m: The second number.
  *
@@ -9,14 +10,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
 
-    while (xor_result)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
 
-    return count;
+	return (count);
 }
