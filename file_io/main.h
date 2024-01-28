f452e606c9acde3400fdc6a8fe57238452822c76
Mutzus <ssmutzus@gmail.com>
2024-01-28 11:54:38 -0800

    Adding a library to main.h

--- file_io/main.h
+++ file_io/main.h
@@ -2,9 +2,9 @@
 #define LIST_H
 
 #include <stddef.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
